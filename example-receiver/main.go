c9bd910bf06e190e12f7426c64833ed2cd70f0eb
Preston Holmes <preston@ptone.com>
2018-12-20 10:03:57 -0800

    comments in example

--- example-receiver/main.go
+++ example-receiver/main.go
@@ -10,6 +10,8 @@ import (
 	"github.com/ptone/promiot"
 )
 
+// to run, set the environment variables like:
+// GCLOUD_PROJECT=ptone-serverless METRICS_SUBSCRIPTION=metric-pull go run main.go
 func main() {
 	project := os.Getenv("GCLOUD_PROJECT")
 	if project == "" {
@@ -19,12 +21,19 @@ func main() {
 	if subscription == "" {
 		log.Fatal("METRICS_SUBSCRIPTION env var must be set")
 	}
+	// A receiver receives encoded metrics on PubSub topic, and unpacks them into a for ready to be gathered on scrape
 	receiver := promiot.NewPromiotReceiver(project, subscription)
+
+	// The receiver is added to the array of default gatherers
 	prometheus.DefaultGatherer = prometheus.Gatherers{
 		prometheus.DefaultGatherer,
 		receiver,
 	}
+
+	// start pulling messages from the pubsub topic
 	go receiver.Receive()
+
+	// expose the collected metrics to be scraped by prometheus
 	http.Handle("/metrics", promhttp.Handler())
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
